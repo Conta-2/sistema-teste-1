bb9512d8fb6938b68db217e273563b48dda2af5b
alex-particular <88983350+alex-particular@users.noreply.github.com>
2023-01-10 20:51:33 -0300

    Alterando para testar o git pull em outro usuário
    Alterando para testar o git pull em outro usuário (Local)

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 console.log("Execução de Testes do GITHUB")
+console.log("Teste de alteração de fonte por outro usuário. Quero ver no PC local pelo git PULL esta mudança")
+console.log("Teste de alteração de fonte por outro usuário. Quero ver no PC local pelo git PULL esta mudança")
