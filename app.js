b34be9c36524dc214884d5d07fab9ec71a0f38df
alex-particular <alex_particular@hotmail.com>
2023-01-10 21:10:54 -0300

    voltando a mudança localmente

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
 console.log("Execução de Testes do GITHUB")
-console.log("Teste de alteração de fonte por outro usuário. Quero ver no PC local pelo git PULL esta mudança")
-console.log("Teste de alteração de fonte por outro usuário. Quero ver no PC local pelo git PULL esta mudança")
